b497a857d985d121624de98d3b20373a0169f13b
Larry Heimann <profh@cmu.edu>
2018-04-20 16:23:34 -0400

    Possible changes to school.rb
    I'm not sure that the initialize method is the best course because I don't know about Salesforce Connect.  If it doesn't work, then the class method or scope is what you need.

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,19 @@
 class School < ApplicationRecord
   has_many :teachers
+	
+  # use the initialize method to do the usual Rails setup but also filter the records for schools 
+  def initialize
+	  super  # need to do this so other parts of AR's initialize method run first
+	  self = self.where(type: "School") # does this work? 'type' used to be a reserved word in earlier Rails
+	  # or 
+	  # self = self.select{ |data| data.type == "school" }
+  end
+	
+  # other option is simply to create a scope that has to be called each time, e.g.,
+  def self.all_schools
+	  self = self.where(type: "School")
+  end
+	
 
 	# this item is synced to Salesforce schools using Heroku Connect
 	self.table_name = "salesforce.account"
